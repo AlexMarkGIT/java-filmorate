b8c1993de10a909692b1d17ed1873b43f789f208
AlexMark <prog.edu@yandex.ru>
2023-10-16 06:48:51 +0300

    ТЗ-11: fix: fix codestyle in storage.FilmStorage

--- src/main/java/ru/yandex/practicum/filmorate/storage/GenreStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/GenreStorage.java
@@ -7,9 +7,13 @@ import java.util.List;
 public interface GenreStorage {
 
     List<Genre> getAllGenres();
+
     Genre getGenreById(Integer id);
+
     void addGenreToFilm(Integer filmId, Integer genreId);
+
     void removeGenresFromFilm(Integer filmId);
+
     List<Genre> getGenresByFilmId(Integer filmId);
 
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/MPAStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/MPAStorage.java
@@ -5,6 +5,8 @@ import ru.yandex.practicum.filmorate.model.MPARating;
 import java.util.List;
 
 public interface MPAStorage {
+
     List<MPARating> getAllMPARatings();
+
     MPARating getMPAById(Integer id);
 }
